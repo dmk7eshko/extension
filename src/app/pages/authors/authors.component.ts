bc977b9f77a311f18ce0446cb772e0d3f7731dda
dmk7eshko <dmitry.semeshko@koniglabs.ru>
2021-09-15 23:47:13 +0200

    after clean code

--- src/app/pages/authors/authors.component.ts
+++ src/app/pages/authors/authors.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, Input, EventEmitter, Output, ChangeDetectorRef  } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ChangeDetectorRef } from '@angular/core';
 import { AuthorsService } from './authors.service';
 import { Author } from '../../entities/author.model';
-import { AuthorDocs } from '../../entities/authorDocs.model';
-import { fromEvent, range, zip } from 'rxjs'; 
+import { fromEvent, range, zip } from 'rxjs';
 import { map, bufferCount, startWith, scan } from 'rxjs/operators';
-// import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-authors',
@@ -12,55 +10,47 @@ import { map, bufferCount, startWith, scan } from 'rxjs/operators';
   styleUrls: ['./authors.component.scss']
 })
 export class AuthorsComponent implements OnInit {
-  
+
   authors: Author[] = [];
-  AuthorDocs  = [];
+  AuthorDocs = [];
   x = 3;
 
-  
+
   constructor(private authorsService: AuthorsService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.authorsService.getData().pipe(
-      // tap(x => console.log(x))
-      
-    ).subscribe((data) => 
-      {
-        this.authors = data;
-        // let x = 3;
-        this.AuthorDocs = this.authors
-          .map(item => item.docs.slice(0, this.x),
-            // .map(item =>  item.name )  
-        )
-        // this.AuthorDocs.sort();
-      });
-      // console.log('x on init', this.x);
+
+    ).subscribe((data) => {
+      this.authors = data;
+      this.AuthorDocs = this.authors
+        .map(item => item.docs.slice(0, this.x),
+      )
+    });
   }
-  // ngDoCheck(){
-  //   console.log('ngDoCheck', this.AuthorDocs[0].length);
-  // }
-  LoadMore(){
+
+  LoadMore() {
     console.log('load more');
-    
+
     let z = this.AuthorDocs[0].length;
     this.AuthorDocs = this.authors
-      .map(item => item.docs.slice(0, z+this.x)
-    )
-  console.log(z)
+      .map(item => item.docs.slice(0, z + this.x)
+      )
+    console.log(z)
 
     const SLICE_SIZE = 50;
     const loadMore$ = fromEvent(document.getElementsByTagName('button')[0], 'click');
     const data$ = range(100);
-       zip(
-         data$.pipe(bufferCount(SLICE_SIZE)),
-         loadMore$.pipe(startWith(0)),
-       ).pipe(
-         map(results => results[0]),
-         scan((acc, chunk) => [...acc, ...chunk], []),
-       ).subscribe({
-         next: v => console.log('v', v),
-         complete: () => console.log('complete'),
-       });
-  
-}
+    zip(
+      data$.pipe(bufferCount(SLICE_SIZE)),
+      loadMore$.pipe(startWith(0)),
+    ).pipe(
+      map(results => results[0]),
+      scan((acc, chunk) => [...acc, ...chunk], []),
+    ).subscribe({
+      next: v => console.log('v', v),
+      complete: () => console.log('complete'),
+    });
+
+  }
 }
--- src/app/pages/authors/authors.service.ts
+++ src/app/pages/authors/authors.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Author } from '../../entities/author.model';
-import {tap, map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorsService {
-    post(author: Author | undefined) {
-    
+  post(author: Author | undefined) {
+
   }
   serviceURL: string = 'https://openlibrary.org/search/authors.json?q=j%20k%20';
-  constructor(private http: HttpClient){ }
-      
-   getData() : Observable<Author[]>{
-        return this.http.get<Author>(this.serviceURL).pipe(
-          map(item => {
-            return [item];
-          })
-);
-}
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<Author[]> {
+    return this.http.get<Author>(this.serviceURL).pipe(
+      map(item => {
+        return [item];
+      })
+    );
+  }
 }
